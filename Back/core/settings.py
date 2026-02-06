2865c096f509967583e5b05e69c176e3030c6c96
mahan <ataeifarmahan@gmail.com>
2026-02-06 15:50:26 +0330

    email info empty

--- Back/core/settings.py
+++ Back/core/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dong.noreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'aoxa jweq bmke stus'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
